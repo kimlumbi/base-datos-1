826d5132e8c39d53bd62fc04a449341918567a0b
KimberlyLumbi <kim.lumbi@estudiantec.cr>
2021-08-26 19:49:12 -0600

    Comments were added

--- practicas/practica-26-08-2021.sql
+++ practicas/practica-26-08-2021.sql
@@ -2,6 +2,7 @@ create database star_wars;
 
 use star_wars;
 
+-- First version of the table
 create table people (
 id int primary key auto_increment,
 name varchar(50),
@@ -11,11 +12,19 @@ eye_color varchar(20),
 gender enum('female', 'male', 'other')
 );
 
+-- Some statements to display the table
 select * from people;
 select id, name from people;
 show create table people;
 describe people;
 
+-- Inserting people w id 
+insert into people(id, name, mass, hair_color, eye_color, gender) values (1, 'Luke Skywalker', 77, 'blond', 'blue', 'male');
+insert into people(id, name, mass, hair_color, eye_color, gender) values (2, 'Leia Skywalker', 70, 'brown', 'brown', 'female');
+-- Inserting people without id (not necessary bc of autoinc)
+insert into people(name, mass, hair_color, eye_color, gender) values ('Han Solo', 80, 'brown', 'brown', 'male');
+
+-- Creating table 'planet'
 create table if not exists planet (
 id int primary key auto_increment,
 name varchar(50),
@@ -29,17 +38,20 @@ surface_water int,
 population long
 );
 
-insert into people(id, name, mass, hair_color, eye_color, gender) values (1, 'Luke Skywalker', 77, 'blond', 'blue', 'male');
-insert into people(id, name, mass, hair_color, eye_color, gender) values (2, 'Leia Skywalker', 70, 'brown', 'brown', 'female');
-insert into people(name, mass, hair_color, eye_color, gender) values ('Han Solo', 80, 'brown', 'brown', 'male');
-
+-- Adding planet scheme instances
 insert into planet (id, name, orbital_period, rotation_period, diameter, climate, gravity, terrain, surface_water,  population) 
 values (1, 'Tatooine', 304, 23, 10465, 'arid', '1 standard', 'desert', 1, 200000);
 
+insert into planet (name, orbital_period, rotation_period, diameter, climate, gravity, terrain, surface_water,  population) 
+values ('Corellia', 304, 23, 10465, 'arid', '1 standard', 'desert', 1, 200000);
+
 select * from planet;
 
+
+-- Droping first version of the table people
 drop table people;
 
+-- Second version of the table, with planet id and constraint
 create table people (
     id int auto_increment,
     name varchar(50),
@@ -51,4 +63,9 @@ create table people (
     primary key(id),
     constraint planet_fk foreign key (planet_id) references planet(id)
 );
+
+-- Inserting people again (w planet id)
+insert into people(name, mass, hair_color, eye_color, gender,  planet_id) values ('Luke Skywalker', 77, 'blond', 'blue', 'male', 1);
+insert into people(name, mass, hair_color, eye_color, gender,  planet_id) values ('Leia Skywalker', 70, 'brown', 'brown', 'female', 1);
+insert into people(name, mass, hair_color, eye_color, gender,  planet_id) values ('Han Solo', 80, 'brown', 'brown', 'male', 2);
 
